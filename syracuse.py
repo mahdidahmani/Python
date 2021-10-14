8df7e4884a2aedad6d0bd9f44fe13ca950c4941f
mdahmani <44885640+mahdidahmani@users.noreply.github.com>
2021-10-14 01:03:01 -0400

    first version Syracuse suite

---
+++ syracuse.py
@@ -0,0 +1,21 @@
+# Calcule de la suite de Syracuse 
+# 1er Programme sur Python 
+
+
+def syracuse(Nombre,Count,Max) :
+    while Nombre != 1 :
+        if Nombre%2 == 0 :
+            Nombre = Nombre // 2
+        else :
+            Nombre = Nombre * 3 + 1
+        print(Nombre)
+        Count += 1 
+        if Nombre >= Max :
+            Max = Nombre
+    return Count,Max
+Nombre = int (input("Entre le Nombre :"))
+Count = int
+Max = int
+print(Nombre)
+Count,Max = syracuse(Nombre,0,0)
+print("La durée du vol pour ",Nombre," est de ",Count,"et son altitude est de",Max,".")

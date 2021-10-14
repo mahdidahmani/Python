678a8711e5e7fc27fb837439382470a4afa78b0b
mdahmani <44885640+mahdidahmani@users.noreply.github.com>
2021-10-14 01:24:18 -0400

    bug fix

--- syracuse.py
+++ syracuse.py
@@ -4,18 +4,20 @@
 
 def syracuse(Nombre,Count,Max) :
     while Nombre != 1 :
+        print(Count," : ",Nombre)
         if Nombre%2 == 0 :
             Nombre = Nombre // 2
         else :
             Nombre = Nombre * 3 + 1
-        print(Count," : ",Nombre)
         Count += 1 
         if Nombre >= Max :
             Max = Nombre
+    else :
+        print(Count," : ",Nombre)
     return Count,Max
 Nombre = int (input("Entre le Nombre :"))
-Count = int
+Count = int 
 Max = int
-print(Count," : ",Nombre)
-Count,Max = syracuse(Nombre,0,0)
+#print(Count," : ",Nombre)
+Count,Max = syracuse(Nombre,1,0)
 print("La durée du vol pour ",Nombre," est de ",Count,"et son altitude est de",Max,".")
